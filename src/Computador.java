11259a9479142dbb26a4454012b18c10e075390b
matias erenchun <matiaserenchun@gmail.com>
2019-01-01 19:45:32 -0300

    suma y multiplica

--- src/Computador.java
+++ src/Computador.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 public class Computador
 {
     private ArrayList<String> rom;
-    private ArrayList<String> ram;
-    Double A;
-    String D;
+    private String[] ram;
+    int A;
+    int D;
     int PC;
     private File file;
     private BufferedReader bf;
@@ -15,11 +15,12 @@ public class Computador
         this.file= new File(file);
         this.bf=new BufferedReader(new FileReader(file));
         this.rom = new ArrayList<>();
-        this.ram = new ArrayList<>();
-        A = new Double(0);
-        D = "0";
+        this.ram = new String[10];
+        this.A = 0;
+        this.D = 0;
         this.PC = 0;
         this.cargarRom();
+        this.cargarRam();
         this.ejecutarPrograma();
     }
 
@@ -33,29 +34,68 @@ public class Computador
 
         }
     }
+    public void cargarRam()
+    {
+        int i=0;
+        while(i<10)
+        {
+            this.ram[i]="0";
+            i++;
+        }
+    }
+
+    public void printRam()
+    {
+        System.out.println("estado de la Ram");
+        for (String s:this.ram)
+        {
+            System.out.println(s);
+        }
+    }
+    public void printRom()
+    {
+        for (String s:this.rom)
+        {
+            System.out.println(s);
+        }
+    }
+
 
     public void ejecutarPrograma()
     {
+        while(this.PC<this.rom.size())
+        {
+
+            this.ejecutarInstruccion();
+            this.printRam();
+        }
+    }
+    public void ejecutarInstruccion()
+    {
         String instruccion = this.rom.get(this.PC);
-        this.PC++;
         String tipoDeInstruccion=instruccion.substring(0,3);
-        System.out.println(tipoDeInstruccion);
+        String guardarADM=instruccion.substring(10,13);
         if(tipoDeInstruccion.equalsIgnoreCase("111"))
         {
-            System.out.println("hola");
+            System.out.println("instruccion del tipo Ram[A]");
+            String answer=this.instruccionesTipoC(instruccion);
+            System.out.println("respuesta de la instruccion "+answer);
+            this.guardarResultado(guardarADM,answer);
+            this.saltos(instruccion,answer);
         }
         else
         {
             this.A=this.binariToDouble(instruccion);
-            System.out.println("000 "+A);
+            this.PC++;
+            System.out.println("valor de A:"+A);
         }
 
     }
 
 
-    public double binariToDouble(String instruccion)
+    public int binariToDouble(String instruccion)
     {
-        double resultado=0;
+        int  resultado=0;
         int i=0;
         while(i<instruccion.length())
         {
@@ -70,60 +110,89 @@ public class Computador
         return resultado;
     }
 
-    public void instruccionesTipoC(String instruccion)
+    public String instruccionesTipoC(String instruccion)
     {
-        double ficheroA;
         String CodigoInstruccion=instruccion.substring(4,10);
+        String answer;
+        Integer aux;
         if(instruccion.charAt(3)=='1')
         {
-            //instrucciones del tipo Ram[A]
-            int a =Integer.parseInt(this.A.toString());
-            ficheroA=Integer.parseInt(this.ram.get(a));
+            System.out.println("instrucciones del tipo Ram[A]=>M");
+            //instrucciones del tipo Ram[A]=>M
             switch (CodigoInstruccion)
             {
                 case "110000"://M
                 {
+                    answer = this.getRamIndex(this.A);
                     break;
                 }
                 case"110001"://!M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=~aux;
+                    answer =aux.toString();
                     break;
                 }
                 case "110011"://-M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=aux*-1;
+                    answer =aux.toString();
                     break;
                 }
                 case "110111"://M+1
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux++;
+                    answer =aux.toString();
                     break;
                 }
                 case "110010" ://M-1
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux--;
+                    answer =aux.toString();
                     break;
                 }
                 case "000010"://D+M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=this.D+aux;
+                    answer =aux.toString();
                     break;
                 }
                 case "010011"://D-M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=this.D-aux;
+                    answer =aux.toString();
                     break;
                 }
                 case "000111"://M-D
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=aux-this.D;
+                    answer =aux.toString();
                     break;
                 }
                 case "000000"://D&M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=aux&this.D;
+                    answer =aux.toString();
                     break;
                 }
                 case "010101"://D|M
                 {
+                    aux =Integer.parseInt(this.getRamIndex(this.A));
+                    aux=aux|this.D;
+                    answer =aux.toString();
                     break;
                 }
 
                 default:
                 {
+                    answer="AAAAAA";
                     System.out.println("error");
                 }
             }
@@ -131,74 +200,203 @@ public class Computador
         }
         else
         {
-            ficheroA=this.A;
+            System.out.println("instrucciones del tipo A");
+            //instrucciones del tipo A
             switch (CodigoInstruccion)
             {
+                case "101010"://0
+                {
+                    answer="0";
+                    break;
+                }
+                case "111111"://1
+                {
+                    answer="1";
+                    break;
+                }
+                case "111010"://-1
+                {
+                    answer="-1";
+                    break;
+                }
+                case "001100"://d
+                {
+                    aux=this.D;
+                    answer=aux.toString();
+                    break;
+                }
                 case "110000"://A
                 {
+                    aux=this.A;
+                    answer=aux.toString();
                     break;
                 }
                 case "001101"://!D
                 {
+                    aux=~this.D;
+                    answer=aux.toString();
                     break;
                 }
                 case"110001"://!A
                 {
+                    aux=~this.A;
+                    answer=aux.toString();
                     break;
                 }
                 case "001111" ://-D
                 {
+                    aux=this.D*-1;
+                    answer=aux.toString();
                     break;
                 }
                 case "110011"://-A
                 {
+                    aux=this.A*-1;
+                    answer=aux.toString();
                     break;
                 }
                 case "011111"://D+1
                 {
+                    aux=this.D+1;
+                    answer=aux.toString();
                     break;
                 }
                 case "110111"://A+1
                 {
+                    aux=this.A+1;
+                    answer=aux.toString();
                     break;
                 }
                 case "001110"://D-1
                 {
+                    aux=this.D-1;
+                    answer=aux.toString();
                     break;
                 }
                 case "110010" ://A-1
                 {
+                    aux=this.A-1;
+                    answer=aux.toString();
                     break;
                 }
                 case "000010"://D+A
                 {
+                    aux=this.D+this.A;
+                    answer=aux.toString();
                     break;
                 }
                 case "010011"://D-A
                 {
+                    aux=this.D-this.A;
+                    answer=aux.toString();
                     break;
                 }
                 case "000111"://A-D
                 {
+                    aux=this.A-this.D;
+                    answer=aux.toString();
                     break;
                 }
                 case "000000"://D&A
                 {
+                    aux=this.D&this.A;
+                    answer=aux.toString();
                     break;
                 }
                 case "010101"://D|A
                 {
+                    aux=this.D|this.A;
+                    answer=aux.toString();
                     break;
                 }
 
                 default:
                 {
+                    answer="AAAAA";
                     System.out.println("error");
                 }
             }
         }
+
+        return answer;
     }
 
+    public void guardarResultado(String ADM,String answer)
+    {
+        if(ADM.charAt(0)=='1')//guardar en A
+        {
+            this.A=Integer.parseInt(answer);
+        }
+        if(ADM.charAt(1)=='1')//guardar en D
+        {
+            this.D=Integer.parseInt(answer);
+        }
+        if(ADM.charAt(2)=='1')//guardar en Ram[A]
+        {
+            this.ram[this.A]=answer;
+        }
+}
 
+    public void saltos(String instruccion, String answer) {
+        String saltos = instruccion.substring(13, 16);
+        int intAnswer = Integer.parseInt(answer);
+
+        //out>0 jump to A
+        if (saltos.equals("001") && intAnswer > 0)
+        {
+            this.PC=this.A;
+        }
+        else
+        {
+            //out==0 jump to A
+            if(saltos.equals("010") && intAnswer == 0)
+            {
+                this.PC=this.A;
+            }
+            else
+            {
+                //out>=0 jump to A
+                if(saltos.equals("011")&&intAnswer>=0)
+                {
+                    this.PC=this.A;
+                }
+                else
+                {
+                    //out<0 jump to A
+                    if(saltos.equals("100")&&intAnswer<0)
+                    {
+                        this.PC=this.A;
+                    }
+                    else
+                    {
+                        //out!=0 jump to A
+                        if(saltos.equals("101")&& intAnswer!=0)
+                        {
+                            this.PC=this.A;
+                        }
+                        else
+                        {
+                            //out<=0 jump to A
+                            if(saltos.equals("110")&& intAnswer<=0)
+                            {
+                                this.PC=this.A;
+                            }
+                            else
+                            {
+                                this.PC++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public String getRamIndex(int index)
+    {
+       return this.ram[index];
+    }
 
 }
